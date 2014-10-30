15b2ef99e6deafc4f6b622ca79fc987d6e63fb37
macikokoro <betwinsouls@gmail.com>
2014-10-29 23:02:35 -0700

    Meanify works

--- js/app.js
+++ js/app.js
@@ -1,35 +1,46 @@
-var meanify = function() {
-var data = [];
+var meanify = function () {
+    var data = [];
+    var numArray = document.getElementById('integers').value.split(' ');
 
-var numArray = document.getElementById('integers').value.split(' ');
+    var findMean = function (numArray) {
+        var sum = 0;
+        for (var i = 0; i < numArray.length; i++) {
+            sum += Number(numArray[i]);
+        }
+        return (sum / numArray.length);
+    };
 
-for (var i = 0; i < numArray.length; i++) {
-        data.push(numArray[i]);
-  }
+    data.push(findMean(numArray));
 
-var width = 420,
-    barHeight = 20;
+    var width = 420,
+        barHeight = 20;
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0, width]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
+    var chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
 
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    var bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) {
+        return "translate(0," + i * barHeight + ")";
+    });
 
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
 
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+    bar.append("text")
+        .attr("x", function (d) {
+        return x(d) - 3;
+    })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function (d) {
+        return d;
+    });
 }
